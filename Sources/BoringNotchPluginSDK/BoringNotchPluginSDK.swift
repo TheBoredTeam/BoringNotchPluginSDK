ed7b7ccfbdd78df3661d240520a603c706dc0e67
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2025-04-06 12:05:01 -0400

    update platforms and add initial version with current protocol and PlaybackState definitions from Boring Notch

--- Sources/BoringNotchPluginSDK/BoringNotchPluginSDK.swift
+++ Sources/BoringNotchPluginSDK/BoringNotchPluginSDK.swift
@@ -1,2 +1,37 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  BoringNotchPluginSDK.swift
+//  BoringNotchPluginSDK
+//
+//  Created by Alexander Greco on 2025-04-05.
+//
+
+import Foundation
+import AppKit
+
+protocol MediaControllerProtocol: ObservableObject {
+    var playbackStatePublisher: Published<PlaybackState>.Publisher { get }
+    func play()
+    func pause()
+    func seek(to time: Double)
+    func nextTrack()
+    func previousTrack()
+    func togglePlay()
+    func isActive() -> Bool
+    func updatePlaybackInfo()
+}
+
+struct PlaybackState {
+    var bundleIdentifier: String
+    var isPlaying: Bool = false
+    var title: String = "I'm Handsome"
+    var artist: String = "Me"
+    var album: String = "Self Love"
+    var currentTime: Double = 0
+    var duration: Double = 0
+    var playbackRate: Double = 1
+    var isShuffled: Bool = false
+    var isRepeating: Bool = false
+    var lastUpdated: Date = Date.distantPast
+    var artwork: Data?
+}
+
